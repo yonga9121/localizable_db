512c558ca4f57955b2d76f51421e25662f56e520
Yonga <jorgeggayon@gmail.com>
2018-02-07 16:33:16 -0500

    Mergin stuff with Richi, The ball Richi

--- test/dummy/app/models/product.rb
+++
@@ -1,7 +0,0 @@
-class Product < ApplicationRecord
-
-  localize [:name, :desc]
-
-  has_many :things, class_name: "Thing", foreign_key: "product_id", inverse_of: :product
-
-end
--- test/dummy/app/models/thing.rb
+++
@@ -1,6 +0,0 @@
-class Thing < ApplicationRecord
-
-  localize [:name]
-
-  belongs_to :product, class_name: "Product", foreign_key: "product_id", inverse_of: :things
-end
