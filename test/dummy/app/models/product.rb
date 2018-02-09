04f753cbbb3c4e621b48f4d756dae42b18f56d30
Yonga <jorgeggayon@gmail.com>
2018-02-09 09:57:42 -0500

    Fix Validators bug for missing attributes. Update Docs

--- test/dummy/app/models/product.rb
+++ test/dummy/app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
 	localize [:name]
 	has_many :things, class_name: "Thing", foreign_key: "product_id"
-	validates :name, presence: true
+	validates :name, :desc, presence: true
 
 	accepts_nested_attributes_for :things
 end
